a20a342285401293b451635800cece4328d2874d
lujialei <lujialei08@163.com>
2023-05-15 16:43:27 +0800

    refactor

--- src/utils/tmsdk.js
+++ src/utils/tmsdk.js
@@ -38,7 +38,7 @@ const Utils = {
   // jsdk
   jsdk(callback, tmLogin) {
     if (window.jssdknum === 0) {
-      let zjrbCp = document.createElement('script');
+      const zjrbCp = document.createElement('script');
       zjrbCp.async = 'async';
       zjrbCp.src =
         'https://activity.tianmunews.com/material/tmWebJssdk/tmJsSdk.js';
@@ -48,15 +48,15 @@ const Utils = {
       zjrbCp.onload = function () {
         if (window.CP && typeof window.CP.ready === 'function') {
           window.CP.ready({
-            debug: false, //可选，是否是开启调试模式, 默认false
-            es6Require: false, //可选，是否需要加载es6库，默认false
+            debug: false, // 可选，是否是开启调试模式, 默认false
+            es6Require: false, // 可选，是否需要加载es6库，默认false
             wxOption: {
-              //可选，内容需要二次分享时请配置此项
-              debug: false, //是否是开启微信调试模式
-              appid: 'wx0511ad48925f257e', //授权的微信公众号的appid  微信二次分享使用，具体使用方式请参照 附录：微信及微信二次分享
-              method: 'getShareSign', //获取微信信息方法，写死
+              // 可选，内容需要二次分享时请配置此项
+              debug: false, // 是否是开启微信调试模式
+              appid: 'wx0511ad48925f257e', // 授权的微信公众号的appid  微信二次分享使用，具体使用方式请参照 附录：微信及微信二次分享
+              method: 'getShareSign', // 获取微信信息方法，写死
               // uploadImage: '', //使用微信录音，图片上传等功能时需要的上传后台服务器地址
-              redirect: location.href.split('#')[0], //回调域名，一般为加载后签名时页面，如跳转可能会签名失败 附录：微信及微信二次分享
+              redirect: location.href.split('#')[0], // 回调域名，一般为加载后签名时页面，如跳转可能会签名失败 附录：微信及微信二次分享
               jsApiList: [
                 'onMenuShareTimeline',
                 'onMenuShareAppMessage',
@@ -81,7 +81,7 @@ const Utils = {
               ], // 分享到微信必填，需要使用的微信JS接口列表，所有JS接口列表见 附录：微信JS接口列表
               openTagList: ['wx-open-launch-app']
             },
-            ready: function (client_info) {
+            ready(client_info) {
               console.log('-----------**********---------');
               let shareUrl =
                 'https://activity.tianmunews.com/material/CW-88zhanlue/index.html';
@@ -89,9 +89,9 @@ const Utils = {
                 shareUrl =
                   'https://activity.tianmunews.com/material/CW-88zhanlue/index.html';
               }
-              //client_info > { client_code: 'web', version: '1.0', detail: ''}  clientInfo 接口返回数据
+              // client_info > { client_code: 'web', version: '1.0', detail: ''}  clientInfo 接口返回数据
               window.CP.setShare({
-                completed: function () {},
+                completed() {},
                 title: '潮闻杯“八八战略”知识竞赛答题',
                 link: shareUrl,
                 imgUrl:
@@ -100,10 +100,10 @@ const Utils = {
               });
               isZjolapp();
             },
-            error: function (error) {
+            error(error) {
               console.log(error);
-              //jssdk运行时报错，但不会阻断程序运行  427569
-              //error > {code: 4000, msg: res.errMsg, data: res}  res为原生报错 微信的报错也在这处理
+              // jssdk运行时报错，但不会阻断程序运行  427569
+              // error > {code: 4000, msg: res.errMsg, data: res}  res为原生报错 微信的报错也在这处理
             }
           });
         }
@@ -117,27 +117,27 @@ const Utils = {
       if (!tmLogin) return;
       // 判断用户是否登陆
       window.CP.isLogin({
-        completed: function (data) {
+        completed(data) {
           window.CP.tools.writelog('get_is_login', data);
           // data === 1 登陆成功，然后获取用户信息
           if (data.is_login === '1') {
             getUserInfo();
           } else {
             window.CP.showLogin({
-              completed: function (data) {
+              completed(data) {
                 // data > {unique_id: '', account_id: '', is_login: ''}
                 window.CP.tools.writelog('show_login', data);
                 if (data.is_login === '1') {
                   getUserInfo();
                 }
               },
-              cpIncompatible: function () {
+              cpIncompatible() {
                 window.CP.tools.writelog('', '此环境不支持此方法');
               }
             });
           }
         },
-        cpIncompatible: function () {
+        cpIncompatible() {
           window.CP.tools.writelog('', '此环境不支持此方法');
         }
       });
@@ -146,7 +146,7 @@ const Utils = {
     // // 是否天目客户端打开
     function isZjolapp() {
       window.CP.getClientInfo({
-        completed: function (data) {
+        completed(data) {
           window.version = data.version;
           data.client_code === 'zjolapp'
             ? zjolInit()
@@ -174,7 +174,7 @@ const Utils = {
     function getUserInfo() {
       // 设备信息
       window.CP.getClientInfo({
-        completed: function (data) {
+        completed(data) {
           // 存储设备信息
           window.appVersion = data.versionName;
           window.deviceNum = data.detail.uuid;
@@ -182,13 +182,13 @@ const Utils = {
         }
       });
       window.CP.getCustomerInfo({
-        completed: function (data) {
+        completed(data) {
           // data > {account_id: '',username: '',head_img: '',nick_name: '',score:'',mobile: '',ref_code:'',ref_user_uid:'',ref_user_code:'',invitation_number: ''}
           window.CP.tools.writelog('get_customer_info', data);
           // 存储用户信息
           window.nick_name =
             data.nick_name.length > 8
-              ? data.nick_name.slice(0, 8) + '...'
+              ? `${data.nick_name.slice(0, 8)}...`
               : data.nick_name;
           window.account_id = data.account_id;
           window.session_id = data.session_id;
@@ -202,7 +202,7 @@ const Utils = {
             mobile: data.mobile
           });
         },
-        cpIncompatible: function () {
+        cpIncompatible() {
           window.CP.tools.writelog('', '此环境不支持此方法');
         }
       });
@@ -221,7 +221,7 @@ const Utils = {
     const isIos13_1 = userAgent.match(/iPhone OS 13_1/gi);
     const isWeixin =
       userAgent.match(/wechat/gi) || userAgent.match(/MicroMessenger/gi);
-    let openLink = `https://tianmunews.com/download.html`;
+    let openLink = 'https://tianmunews.com/download.html';
 
     const iosyybLink = `https://a.app.qq.com/o/simple.jsp?pkgname=cn.com.zjol&ios_scheme=${encodeURIComponent(
       iosLink
@@ -280,8 +280,8 @@ const Utils = {
   // 用户手机号脱敏
   desensitization(number) {
     if (!number) return '';
-    let matching = /(\d{3})\d*(\d{4})/;
-    let result = number.replace(matching, '$1***$2');
+    const matching = /(\d{3})\d*(\d{4})/;
+    const result = number.replace(matching, '$1***$2');
     return result;
   },
 
@@ -293,7 +293,7 @@ const Utils = {
       task_id: id,
       task_type: type
     };
-    const ZBJTJSBridge = window.ZBJTJSBridge;
+    const { ZBJTJSBridge } = window;
     ZBJTJSBridge.invoke(
       'jumpToNativePage',
       JSON.stringify(obj),
@@ -308,13 +308,10 @@ const Utils = {
     switch (arguments.length) {
       case 1:
         return parseInt(Math.random() * minNum + 1, 10);
-        break;
       case 2:
         return parseInt(Math.random() * (maxNum - minNum + 1) + minNum, 10);
-        break;
       default:
         return 0;
-        break;
     }
   }
 };
